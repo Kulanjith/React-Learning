6b093ffd713137b262bc3f3cbc63c98c4c5d40c1
devin <devink@hsenidmobile.com>
2021-10-15 21:10:25 +0530

    Finish the assignment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { Header } from './components/Header';
+import { TableComponent } from './components/TableComponent';
+import {useState, useEffect} from 'react'
+import { Pagination } from './components/Pagination';
+import {Route, BrowserRouter as Router, Switch} from "react-router-dom"
+import { UserComponent } from './components/UserComponent';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const [totalPages, setTotalPages] = useState(1);
+  const [pageNumber, setPageNumber] = useState(1);
+  
+  useEffect(()=>{
+      const getUsers = async ()=>{
+         const userList = await fetchUsers();
+         setUsers(userList.data);
+         setTotalPages(userList.total_pages);
+      }
+      getUsers();
+  },[pageNumber]);
+  
+  // fetch users
+  const fetchUsers = async() =>{
+    const res = await fetch(`https://reqres.in/api/users?page=${pageNumber}`);
+    const data = await res.json();
+    return data;
+  }
+  
+  // pagination
+  const paginate = (num) => {
+      setPageNumber(num);
+  }
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path={"/users/:id"}>
+            <Header text1="User" text2="Profiles"/>
+            <UserComponent/>
+          </Route>
+          <Route path="/">
+            <Header text1="User" text2="Management"/>
+            <TableComponent users={users}/>
+            <Pagination totalpages={totalPages} fetchUsers={paginate}/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    
   );
 }
 
